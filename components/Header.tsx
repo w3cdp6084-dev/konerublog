1b0d125bb08340a896b534bd01d868a3b27ff5dc
森裕輔 <yusukew3@gmail.com>
2022-01-19 08:43:38 +0900

    Header 追加

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import NaviGation from './NaviGation';
+// import { ChakraProvider } from '@chakra-ui/react';
+export const Header = () => {
+    return (
+      <>
+        <NaviGation />
+      </>
+    );
+};
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { client } from "../libs/client";
-
-export default function Home({ blog }) {
+import { Pagination } from '../components/Pagination';
+import { Header } from '../components/Header';
+export default function Home({ blog, totalCount}) {
   return (
     <div>
+      <div>
+        <Header />
+      </div>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -13,17 +17,24 @@ export default function Home({ blog }) {
           </li>
         ))}
       </ul>
+      <Pagination totalCount={totalCount} />
     </div>
   );
 }
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
+  const key = {
+    headers: {'X-MICROCMS-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://mochimochi.microcms.io/api/v1/blog?offset=0&limit=5', key)
+    .then(res => res.json())
+    .catch(() => null);
 
   return {
     props: {
       blog: data.contents,
+      totalCount: data.totalCount
     },
   };
 };
