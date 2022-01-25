537dc332a654cb8082bb0ef914081256cbaa1bec
森裕輔 <yusukew3@gmail.com>
2022-01-26 07:26:48 +0900

    about style

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/About.module.scss";
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import { Social } from "components/Social";
 import Image from 'next/image';
 import Link from 'next/link';
 import { Box } from '@chakra-ui/react';
@@ -11,26 +12,27 @@ export const About = () => {
         <Header />
       </div>
       <main className={styles.main}>
-      <div className={styles.logoWrap}>
-        <div className={styles.logoShadow}>
-            <Image src="/image/brand_logo.png" width={100} height={100} alt="logo" />
+        <div className={styles.logoWrap}>
+            <div className={styles.logoShadow}>
+                <Image src="/image/brand_logo.png" width={100} height={100} alt="logo" />
+            </div>
         </div>
-      </div>
-      <dl>
-          <dt>KONEKONE</dt>
-          <dd>Yusuke Mori</dd>
-          <dd>Designer/Developer</dd>
-      </dl>
-      <p>
-          KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
-ブランディングとコンセプトメイクの設計を大切にし、モノづくりをおこなっています。<br />
-デザイン・プログラムでいろいろな実験を個人で行ってますので、こちらはそのnoteにもなっています。
-      </p>
-      <Box>
-        <Link href={`https://konekone.org/`}>PortForio</Link>
-      </Box>
+        <dl>
+            <dt>KONEKONE</dt>
+            <dd>Yusuke Mori</dd>
+            <dd>Designer/Developer</dd>
+        </dl>
+        <p>
+            KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
+            ブランディングとコンセプトメイクの設計を大切にし、モノづくりをおこなっています。<br />
+            デザイン・プログラムでいろいろな実験を個人で行ってますので、こちらはそのnoteにもなっています。
+        </p>
+        <Box className={styles.pfLink}>
+            <Link href={`https://konekone.org/`}>PortForio</Link>
+        </Box>
+        <Social />
       </main>
-      <Footer />
+        <Footer />
     </div>
   );
 }
